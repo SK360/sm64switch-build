4b014fbd2c48107cca8f7a92750d99fead624008
Matt Simmons <Matt.simmons@gmail.com>
2020-05-18 10:06:38 -0400

    Updated build script

--- build.sh
+++ build.sh
@@ -2,6 +2,11 @@
 
 source /opt/devkitpro/switchvars.sh
 
+REGION=$(ls /mnt/n64/*.z64 |grep -oP '(?<=[.])\w+(?=[.])')
+[[ -z "$camera" ]] || export cam=BETTERCAMERA=$camera
+[[ -z "$drawdistance" ]] || export $draw=NODRAWINGDISTANCE=$drawdistance
+[[ -z "$extoptions" ]] || export $extopt=EXT_OPTIONS_MENU=$extoptions
+
 cd /home
 # Clone SM64PC Repo
 git clone https://github.com/fgsfdsfgs/sm64pc.git
@@ -10,7 +15,7 @@ cd sm64pc
 
 git checkout switch
 
-cp /mnt/n64/baserom.us.z64 /home/sm64pc/baserom.us.z64
+cp /mnt/n64/baserom.$REGION.z64 /home/sm64pc/baserom.$REGION.z64
 echo "Building SM64 for Switch"
-make -j4 TARGET_SWITCH=1 BETTERCAMERA=$camera
-cp /home/sm64pc/build/us_nx/sm64*nro /mnt/n64
+make $cam $draw $extopt -j4
+cp /home/sm64pc/build/$REGION_nx/sm64*nro /mnt/n64
