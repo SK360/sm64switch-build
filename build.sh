da47d66037c45741807ca96f76ea98c9cf0d706d
Matt Simmons <Matt.simmons@gmail.com>
2020-05-18 11:41:16 -0400

    Updated build script

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
+echo "Running switchvars.sh"
 source /opt/devkitpro/switchvars.sh
 
-REGION=$(ls /mnt/n64/*.z64 |grep -oP '(?<=[.])\w+(?=[.])')
+region=$(ls /mnt/n64/*.z64 |grep -oP '(?<=[.])\w+(?=[.])')
+echo "Region identified as $region"
 [[ -z "$camera" ]] || export cam=BETTERCAMERA=$camera
 [[ -z "$drawdistance" ]] || export draw=NODRAWINGDISTANCE=$drawdistance
 [[ -z "$extoptions" ]] || export extopt=EXT_OPTIONS_MENU=$extoptions
@@ -15,7 +16,8 @@ cd sm64pc
 
 git checkout switch
 
-cp /mnt/n64/baserom.$REGION.z64 /home/sm64pc/baserom.$REGION.z64
-echo "Building SM64 for Switch"
+cp /mnt/n64/baserom.$region.z64 /home/sm64pc/baserom.$region.z64
+echo "Building SM64 for Switch using options:"
+echo "make $cam $draw $extopt -j4"
 make $cam $draw $extopt -j4
-cp /home/sm64pc/build/$REGION_nx/sm64*nro /mnt/n64
+cp /home/sm64pc/build/"$region"_nx/sm64*nro /mnt/n64
